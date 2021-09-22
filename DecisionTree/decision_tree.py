46b8ca30c2471d1c768c15265c8cb027b933bd2b
jackmachara <55965751+jackmachara@users.noreply.github.com>
2021-09-22 16:43:26 -0600

    correct the tree building, nodes have mcl

--- DecisionTree/decision_tree.py
+++ DecisionTree/decision_tree.py
@@ -1,12 +1,18 @@
 import math
+#Node class
+#data is the most common label if it is a leaf node, else it is the variable to split on
+#mcl is the most common label of the subset of data for this branch
+#children is a dictionary of the children nodes of the node
 class Node:
     def __init__(self,data):
         self.data = data
+        self.mcl = None
         self.children = {}
 
 class Algorithms:
     
-    
+    #reads the file specified in filepath 
+    # returns an array of dictionaries with all of the line data and the dictionary index for the label
     def read_file(filepath):
         data_array = []
         datafile = open(filepath,'r')
@@ -21,14 +27,14 @@ class Algorithms:
         datafile.close()
         return data_array,len(data_array[0])-1
     
-    
+    ##Makes an array of the attributes for the data dictionaries
     def make_attributes_array(i):
         return_array = []
         for counter in range(0,i):
             return_array.append(counter)
         return return_array
     
-    
+    #Makes a data subset of the current data with the value of the specifies 'variable' as val
     def data_subset(data,variable,val):
         new_data = []
         for dict in data:
@@ -36,7 +42,7 @@ class Algorithms:
                 new_data.append(dict)
         return new_data
     
-    
+    #makes a dictionary of the variables and how mny values there are of each
     def make_variable_dict(data,variable):
         ret_dict = {}
         for dict in data:
@@ -46,7 +52,7 @@ class Algorithms:
                 ret_dict[dict[variable]] = 1
         return ret_dict
     
-    
+    ##returns the most common value in the dictionary, used on make_vaiable_dict dictionaries
     def get_most_common(dict):
         keys = list(dict.keys())
         most_common = keys[0]
@@ -57,7 +63,7 @@ class Algorithms:
                 num = dict[keys[i]]
         return most_common
     
-    
+    #checks to see if all the data values have the same label
     def label_check(data,label):
         first_label = data[0][label]
         for dict in data:
@@ -65,7 +71,8 @@ class Algorithms:
                 return False
         return True
     
-    
+    #returns the entropy of the variable that dict has the values of
+    #size is the total number of values
     def get_entropy(size,dict):
         entropy = 0
         for key in dict.keys():
@@ -73,20 +80,20 @@ class Algorithms:
             entropy -= (percent*math.log2(percent))
         return entropy
     
-    
+    #returns the majority error of the varaible that dict has values of
     def get_majority_error(size,dict):
         majority_error = 0
         mcv = Algorithms.get_most_common(dict)
         return (size-dict[mcv])/size
     
-    
+    #returns the gini index of the varaible that dict has values of
     def get_gini(size,dict):
         gini = 1
         for key in dict:
             gini -= (dict[key]/size)**2
         return gini
     
-    
+    #finds attribute that provides the best gain to split on in data using entropy
     def find_best_gain_e(data,attributes,label):
         label_dict = Algorithms.make_variable_dict(data,label)
         set_entropy = Algorithms.get_entropy(len(data),label_dict)
@@ -104,7 +111,7 @@ class Algorithms:
                 gain_attribute = attribute
         return gain_attribute
     
-    
+    #finds attribute that provides the best gain to split on in data using majority error
     def find_best_gain_me(data,attributes,label):
         label_dict = Algorithms.make_variable_dict(data,label)
         set_majority_error = Algorithms.get_majority_error(len(data),label_dict)
@@ -122,7 +129,7 @@ class Algorithms:
                 gain_attribute = attribute
         return gain_attribute
     
-    
+    #finds attribute that provides the best gain to split on in data using gini index
     def find_best_gain_g(data,attributes,label):
         label_dict = Algorithms.make_variable_dict(data,label)
         set_gini = Algorithms.get_gini(len(data),label_dict)
@@ -140,10 +147,11 @@ class Algorithms:
                 gain_attribute = attribute
         return gain_attribute
     
-    
+    #builds the tree using the data provided. returns the root node
     def build_tree(data,attributes,label,node,depth,split):
-        if depth == 1 or len(attributes) == 0:
-            node.data = Algorithms.get_most_common(Algorithms.make_variable_dict(data,label))
+        node.mcl = Algorithms.get_most_common(Algorithms.make_variable_dict(data,label))
+        if depth == 1 or len(attributes) == 0:  
+            node.data = node.mcl  
             return node
         elif Algorithms.label_check(data,label):
             node.data = data[0][label]
@@ -169,7 +177,7 @@ class Algorithms:
                 node.children[key] = newNode
             return node
     
-    
+    #returns an array of trees and labels with every combination from the splits and depths on the file
     def build_tree_array(splits,depths,file):
         tree_array = []
         label = []
@@ -180,6 +188,7 @@ class Algorithms:
                 tree_array.append(Algorithms.build_tree(data_label[0],attribute_array,data_label[1],Node(None),depth,split))
                 label.append(data_label[1])
         return tree_array,label
+    # returns if the value of the label is the same as the predicted value from the tree.
     def predict_label(tree,dict,label):
         dict_label = dict[label]
         current_node = tree
@@ -189,8 +198,10 @@ class Algorithms:
             if branch in current_node.children.keys():
                 current_node = current_node.children[branch]
             else:
-                return False
+                return dict_label == current_node.mcl
         return current_node.data == dict_label
+    #returns the number of lines that don't get the correct prediction from the tree
+    #var has an array of trees as its 0 index, and the array of labels in its second index
     def get_prediction_errors(var,testing_data):
         i = 0
         return_array = []
@@ -217,6 +228,8 @@ i = 0
 for file in test_files:
     testing_data.append(Algorithms.read_file(file)[0])
 error_array = []
+#gets the errors of the predictions into a 2d array
+
 for data in testing_data:
     error_array.append(Algorithms.get_prediction_errors(tree_array__label,data))
 f = open("./Decision_Tree_Data.txt","w")
