da5462c6b52314dea0559f464d53a1e13a9a4863
jackmachara <55965751+jackmachara@users.noreply.github.com>
2021-10-28 20:40:51 -0600

    run.sh push

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "linear Regression"
+cd LinearRegression
+python linear_regression.py
+cd ..
+echo "Ensemble Learning"
+cd EnsembleLearning
+#python emsemble_learning.py
+echo "Data now avaliable in Data folders"
+read
